8fc860cbfc1ea12f6365180c493afa4e41bad97d
Mehdi Rad <mahdirad0@gmail.com>
2022-01-26 11:52:18 +0330

    Add bulma with sass support

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -4,10 +4,7 @@ function App() {
   return (
     <div className="map">
       <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
+        <TileLayer url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png" />
         <Marker position={[51.505, -0.09]}>
           <Popup>
             A pretty CSS3 popup. <br /> Easily customizable.
